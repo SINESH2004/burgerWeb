d165f44131efbb4e7dded781ab66c6f43d62216f
SINESH2004 <138645911+SINESH2004@users.noreply.github.com>
2023-12-12 10:54:47 +0530

    Setting up routing and setting the background image correctly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
-    component:HomePageComponent,
-    path:'home'
+    path:'home', component: HomePageComponent
+  }
+  ,
+  {
+    path:'',component: LoginPageComponent
   }
 ];
 
